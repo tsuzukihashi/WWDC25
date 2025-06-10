e47dec0f803a3d50b3698475db630e16531d487b
Ryo Tsuzukihashi <ryo.tsuzukihashi@zozo.com>
2025-06-11 01:21:28 +0900

    Create AlarmCreateScreen.swift

---
+++ WWDC25/AlarmKit/AlarmCreateScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmCreateScreen.swift
+//  WWDC25
+//
+//  Created by ryo.tsuzukihashi on 2025/06/11.
+//
+
+import SwiftUI
+
+struct AlarmCreateScreen: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+#Preview {
+  AlarmCreateScreen()
+}
